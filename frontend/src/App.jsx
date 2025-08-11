7f9487ba99d4efa64ba9b59d43072fdded2455bc
Rinci <rinciatrey@gmail.com>
2025-08-12 01:36:41 +0530

    Redirecting of links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,6 @@
-import { useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import { Toaster } from 'react-hot-toast'
-import LandingPage from './components/mainPages/LandingPage'
-import AboutPage from './components/mainPages/AboutPage'
-import RegisterPage from './components/mainPages/RegisterPage'
-import DashboardPage from './components/mainPages/DashboardPage'
-import LoginPage from './components/mainPages/LoginPage'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { getApps } from './utils/helper'
 
 
 
@@ -16,22 +8,12 @@ import LoginPage from './components/mainPages/LoginPage'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+const CurrentApp = getApps();
 
   return (
-    //parent component that stores all the routes
     <>
     <Router>  
-    <NavBar/>   
-    <Toaster position='bottom-center'/>      
-       <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/about' element= {<AboutPage/>}/>
-        <Route path='/register' element={<RegisterPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/dashboard' element={<DashboardPage/>}/>
-       </Routes>
-       <Footer/>
+        <CurrentApp/>
     </Router>
     </>
   )
